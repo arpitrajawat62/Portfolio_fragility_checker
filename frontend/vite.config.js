bebd504b1b5ab4a1acdab35765a99c105ba7a801
Arpit-Rajawat <Arpitsingh62650@gmail.com>
2026-06-19 11:07:12 +0530

    Add frontend vite.config file

---
+++ frontend/vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/Fragility_score': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/portfolio': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  css: {
+    devSourcemap: true,
+    modules: false
+  }
+})
